e89e7883501cf1839f05cc20d82089defaeef55c
Kamran <83241419+NarmakTwo@users.noreply.github.com>
2023-08-27 09:56:34 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs').promises;
+const moment = require('moment');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+const messagesFilePath = 'temporary-messages.json';
+fs.writeFile(messagesFilePath, '[]', 'utf8')
+  .then(() => console.log('Created messages file'))
+  .catch(err => console.error('Error creating messages file:', err));
+
+app.post('/send', async (req, res) => {
+  const message = req.body.message;
+  const timestamp = moment().format();
+
+  try {
+    const existingMessages = await fs.readFile(messagesFilePath, 'utf8');
+    const parsedMessages = JSON.parse(existingMessages);
+    parsedMessages.push({ message, timestamp });
+    await fs.writeFile(messagesFilePath, JSON.stringify(parsedMessages), 'utf8');
+    res.status(200).json({ message: 'Message stored successfully.' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to store the message.' });
+  }
+});
+
+app.get('/retrieve', async (req, res) => {
+  try {
+    const existingMessages = await fs.readFile(messagesFilePath, 'utf8');
+    const parsedMessages = JSON.parse(existingMessages);
+    const timestampThreshold = moment().subtract(48, 'hours').format();
+    const filteredMessages = parsedMessages.filter(message => moment(message.timestamp).isAfter(timestampThreshold));
+    res.status(200).json(filteredMessages);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve messages.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
